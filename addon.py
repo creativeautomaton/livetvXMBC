099b6e3e96ef0889efc1b00531c02e9aa7f154e7
creativeautomaton <creativeautomaton@users.noreply.github.com>
2015-12-03 22:56:56 -0600

    Update addon.py

--- addon.py
+++ addon.py
@@ -4,8 +4,7 @@ import xbmcgui
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
-line1 = "Hello World!"
-line2 = "We can write anything we want here"
-line3 = "Using Python"
+line1 = "Hello LiveTv.sx World!"
+line2 = " Soon this will have a list os links." 
  
-xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+xbmcgui.Dialog().ok(addonname, line1, line2)
